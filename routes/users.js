5b6c473b008fe1b42e639728bdfd03148322ef2c
Bhirawa Mbani <bhirmbani@gmail.com>
2017-04-25 17:08:01 +0700

    auth for get user if admin done

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/users');
+const auth = require('../helpers/auth');
 
 router.post('/signup', userController.signUp);
-router.get('/', userController.getAll);
+router.post('/signin', userController.signIn);
+router.get('/', auth.adminAuth, userController.getAll);
 
 module.exports = router;
