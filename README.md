724985a5cbceb38294cf7d62ef0a34b0a3eb12d2
haryanawisnu <arya.wisnu05@gmail.com>
2017-04-25 14:54:18 +0700

    add template and add signup

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # api-auth
+
+Author : Haryana wisnu
+
+Create Date : Selasa, 25 April 2017
+
+Description : API Auth dengan CRUD dan Authentication password dengan token
+
+Acces via localhost:3000/api/users
+
+Install Step :
+- create folder api-auth
+- npm init
+- express .
+- npm install
+- npm install --save sequelize sequelize-cli pg jsonwebtoken password-hash
+- sequelize init
+- sequelize model:craete --name Users --attribute username:string,password:string,role:string,email:string
+- sequelize db:migrate
+- sequelize seed:craete --name seed-users-dummy
+- sequelize db:seed --seed seed-users-dummy
+
+
+| Route | HTTP |Description|
+| ------ | ------ |------ |
+|/api/signup       |   POST   |      sign up with new user info|
+|/api/signin       |   POST   |      sign in while get an access token based on credentials|
+|/api/users       |   GET   |      get all the users(admin)|
+|/api/users/:id   |   GET      |   get a single users(admin and Authenticaticated user)|
+|/api/users       |   POST     |   create a user (admin only)|
+|/api/users/:id   |   DELETE    |  delete a user (admin only)|
+|/api/users/:id   |   PUT        | update a user with new info (admin and Authenticated user)|
---
+++ controllers/usersControllers.js
@@ -0,0 +1,105 @@
+var db = require('../models');
+var passwordHash = require('password-hash');
+
+module.exports = {
+  getall: (req, res, next) => {
+    db.User.findAll().then(ArrResult => {
+      res.json(ArrResult);
+    })
+  },
+  getone: (req, res, next) => {
+    let id = req.params.id;
+    db.User.findById(id).then(Result => {
+      res.json(Result);
+    })
+  },
+  create: (req, res, next) => {
+    let username = req.body.username;
+    let password = req.body.password;
+    let role = req.body.role;
+    let email = req.body.email;
+    console.log(`username ${username}, password ${password}, role ${role}, email ${email}`);
+    db.User.create({
+      username: username,
+      password: password,
+      role: role,
+      email: email
+    }).then(Result => {
+      res.json(Result);
+    }).catch(err => {
+      res.send(err);
+    })
+  },
+  delete: (req, res, next) => {
+    let id = req.params.id;
+    db.User.destroy({
+      where: {
+        id: id
+      }
+    }).then(Result => {
+      res.json(Result);
+    }).catch(err => {
+      res.json(err);
+    })
+  },
+  update: (req, res, next) => {
+    let id = req.params.id;
+    let username = req.body.username;
+    let password = req.body.password;
+    let role = req.body.role;
+    let email = req.body.email;
+    db.User.update({
+      username: username,
+      password: password,
+      role: role,
+      email: email
+    }, {
+      where: {
+        id: id
+      }
+    }).then(Result => {
+      res.json(Result);
+    }).catch(err => {
+      res.json(err);
+    })
+  },
+  signup: (req, res, next) => {
+    let username = req.body.username;
+    var password = passwordHash.generate(req.body.password);
+    let role = req.body.role;
+    let email = req.body.email;
+    console.log(`username ${username}, password ${password}, role ${role}, email ${email}`);
+    db.User.create({
+      username: username,
+      password: password,
+      role: role,
+      email: email
+    }).then(Result => {
+      res.json(Result);
+    }).catch(err => {
+      res.send(err);
+    })
+  }
+  signup: (req, res, next) => {
+    let username = req.body.username;
+    let password = req.body.password;
+    console.log(`username ${username}`);
+    db.User.findOne({
+      username: username
+    }).then(user => {
+      if (!user) {
+        res.json({
+          success: false,
+          message: 'Authentication failed. User not found.'
+        });
+      } else if (user) {
+        if (passwordHash.verify(password, user.password)) {} else {
+          res.json({
+            success: false,
+            message: 'Authentication failed. Wrong password.'
+          });
+        }
+      }
+    })
+  }
+}
