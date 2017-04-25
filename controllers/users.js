87fa29f43f08be017e74886b876bbf26f0e0de8e
Bhirawa Mbani <bhirmbani@gmail.com>
2017-04-25 14:40:17 +0700

    initial commit

---
+++ controllers/users.js
@@ -0,0 +1,25 @@
+const db = require('../models');
+const methods = {};
+
+methods.getAll = (req, res) => {
+  db.User.findAll()
+  .then(users => {
+    res.send(users);
+  })
+  .catch(err => {
+    res.send(err);
+  })
+}
+
+methods.findId = (req, res) => {
+  db.User.findById(req.params.id)
+  .then(user => {
+    res.send(user);
+  })
+  .catch(err => {
+    res.send(err);
+  })
+}
+
+
+module.exports = methods;
